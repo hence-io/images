f76116f80ef75a243dc66b94c4814d8c6b7ef285
Clint Beacock <clint@therefore.ca>
2015-07-24 14:55:38 -0400

    trying to work out network issues between nginx and php

--- nginx/helpers/build_and_run.sh
+++ nginx/helpers/build_and_run.sh
@@ -2,7 +2,8 @@
 
 docker build -t hence/nginx .
 docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
-docker run --rm -it -p 80:80 --name nginx hence/nginx
+# docker run --rm -it -p 80:80 --name nginx hence/nginx
+docker run --rm -it -p 80:80 -v /hence/projects/drupal/public:/app --name nginx hence/nginx
 # docker run --rm -it -e CONFIG_PRESET=php -p 80:80 --name nginx hence/nginx
 # docker run --rm -it -e CONFIG_PRESET=drupal7 -p 80:80 --name nginx hence/nginx
 # docker run --rm -it -e CONFIG_PRESET=angular -p 80:80 --name nginx hence/nginx
