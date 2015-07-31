153c9ace971fd3578d3e3d87000a5ef6df3f3242
Clint Beacock <clint@therefore.ca>
2015-07-31 13:08:07 -0400

    added env var to control innodb_log_file_size

--- mariadb/post-install.sh
+++ mariadb/post-install.sh
@@ -7,8 +7,6 @@ mkdir -p $HENCE_APP_DIR/logs
 
 cp -R /etc/mysql/* $HENCE_APP_DIR/conf.defaults
 
-cd $HENCE_APP_DIR
-
 # Setup mount point symlinks
 ln -s $HENCE_APP_DIR/conf $HENCE_APP_VOL_PREFIX/conf
 ln -s $HENCE_APP_DIR/data $HENCE_APP_VOL_PREFIX/data
