fc08f88b002801fa83cfa2fbda016872278cf6fc
Clint Beacock <clint@therefore.ca>
2015-08-04 16:14:36 -0400

    Added persistent data to solr image

--- solr/post-install.sh
+++ solr/post-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p $HENCE_APP_DIR/conf
+mkdir -p $HENCE_APP_DIR/data
 mkdir -p $HENCE_APP_DIR/conf.defaults/basic/conf
 mkdir -p $HENCE_APP_DIR/logs
 
@@ -8,4 +9,5 @@ cp -R /opt/solr/server/solr/configsets/basic_configs/conf/* $HENCE_APP_DIR/conf.
 
 # Setup mount point symlinks
 ln -s $HENCE_APP_DIR/conf $HENCE_APP_VOL_PREFIX/conf
+ln -s $HENCE_APP_DIR/data $HENCE_APP_VOL_PREFIX/data
 ln -s $HENCE_APP_DIR/logs $HENCE_APP_VOL_PREFIX/logs
