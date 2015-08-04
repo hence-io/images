c763b86b45e116e5edbb1fb54e495015a3783305
Clint Beacock <clint@therefore.ca>
2015-08-04 13:48:28 -0400

    changed file permissions on images to correspond to permissions for mounted images in vagrant vm

--- solr/helpers/build_and_run.sh
+++ solr/helpers/build_and_run.sh
@@ -2,5 +2,5 @@
 
 docker build -t hence/solr .
 docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
-docker run --rm -it -p 8983:8983 --name solr hence/solr
-# docker run --rm -it -e CONFIG_PRESET=drupal/search_api/5.x -e CONFIG_PRESET_PATH=drupal -p 8983:8983 --name solr hence/solr
+# docker run --rm -it -p 8983:8983 --name solr hence/solr
+docker run --rm -it -e CONFIG_PRESET=drupal/search_api/5.x -e CONFIG_PRESET_PATH=drupal -p 8983:8983 --name solr hence/solr
