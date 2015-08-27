7d3d6a3ba609ffb3010115744eadaa9a6abf2d65
Clint Beacock <clint@therefore.ca>
2015-08-27 15:13:10 -0400

    NGINX: added ssl support

--- nginx/helpers/build_and_run.sh
+++ nginx/helpers/build_and_run.sh
@@ -3,8 +3,9 @@
 docker build -t hence/nginx .
 docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
 # docker run --rm -it -p 80:80 --name nginx hence/nginx
-docker run --rm -it -p 80:80 -v /hence/projects/drupal/public:/app --name nginx hence/nginx
+# docker run --rm -it -p 80:80 -v /hence/projects/drupal/public:/app --name nginx hence/nginx
 # docker run --rm -it -e CONFIG_PRESET=php -p 80:80 --name nginx hence/nginx
+docker run --rm -it -v /etc/ssl:/etc/ssl -e CONFIG_PRESET=nodejs -e NGINX_SSL_CERT_FILE=ssl-cert-snakeoil.pem -e NGINX_SSL_CERT_KEY_FILE=ssl-cert-snakeoil.key -e ENABLE_SSL=1 -p 80:80 --name nginx hence/nginx
 # docker run --rm -it -e CONFIG_PRESET=drupal7 -p 80:80 --name nginx hence/nginx
 # docker run --rm -it -e CONFIG_PRESET=angular -p 80:80 --name nginx hence/nginx
 # docker run --rm -it -e CONFIG_PRESET=nodejs -p 80:80 --name nginx hence/nginx
