a5c219f2890e2c51a1cb7e14f2f23cce7a2c96da
Clint Beacock <clint@therefore.ca>
2015-09-04 16:45:59 -0400

    Added a php/drupal7 image with drush and phpmyadmin, and updated the php and nginx images to suit it

---
+++ php/drupal7/helpers/build_and_run.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+docker build -t hence/php-drupal7 .
+docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
+docker run --rm -it -p 9000:9000 -e MYSQL_HOST=db --name php-drupal7 hence/php-drupal7
