40373ebc1118b118986a54020be4b24c1b980df0
Clint Beacock <clint@therefore.ca>
2016-01-04 14:52:50 -0500

    Set max_input_vars to be configurable via an ENV var

--- php/helpers/build_and_run.sh
+++ php/helpers/build_and_run.sh
@@ -2,4 +2,4 @@
 
 docker build -t hence/php .
 docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
-docker run --rm -it -p 9000:9000 --name php hence/php
+docker run --rm -it -p 9000:9000 -e PHP_MAX_INPUT_VARS=1500 --name php hence/php
