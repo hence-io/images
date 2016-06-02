c30d1e96731ae4bff38556e3f342cf322c8dd21c
Clint Beacock <clint@therefore.ca>
2016-06-02 15:50:14 -0400

    Fix to cause newrelic to only install on first build

--- php/helpers/build_and_run.sh
+++ php/helpers/build_and_run.sh
@@ -2,4 +2,4 @@
 
 docker build -t hence/php .
 docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
-docker run --rm -it -e NR_APP_NAME="Custom App Name" -e NR_INSTALL_SILENT=true -e NR_INSTALL_KEY=0c6d5e110c5259e1320208f5e1efed0348a628bf --name php hence/php
+docker run -d -it -e NR_APP_NAME="Custom App Name" -e NR_INSTALL_KEY=0c6d5e110c5259e1320208f5e1efed0348a628bf --name php hence/php
