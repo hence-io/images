2e4a859a88cb083bb979166f9be4826f89036650
Clint Beacock <clint@therefore.ca>
2016-07-21 13:00:35 -0400

    Upgraded to Mariadb 10.1

--- mariadb/helpers/build_and_run.sh
+++ mariadb/helpers/build_and_run.sh
@@ -2,4 +2,4 @@
 
 docker build -t hence/mariadb .
 docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
-docker run --rm -it -e MARIADB_USER=admin -e MARIADB_PASSWORD=password -e MARIADB_DATABASE=drupal -p 3306:3306 --name mariadb hence/mariadb
+docker run --rm -it -e MARIADB_USER=root -e MARIADB_PASSWORD=root -e MARIADB_DATABASE=drupal -p 3307:3306 --name mariadb hence/mariadb
