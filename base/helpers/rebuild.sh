a93826f5b2cbfca8f455a7c4f12b3ca3f32f7ab5
Clint Beacock <clint@therefore.ca>
2016-07-21 12:26:47 -0400

    Upgraded base to alpine 3.4

--- base/helpers/rebuild.sh
+++ base/helpers/rebuild.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build --no-cache -t hence/base:3.2 .
-docker build --no-cache -t hence/base:3.2-onbuild ./onbuild/
+docker build --no-cache -t hence/base:3.4 .
+docker build --no-cache -t hence/base:3.4-onbuild ./onbuild/
